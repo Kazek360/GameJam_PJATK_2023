de947c9ac415ebe2eeb5576a1d4e0d026b45b31d
Oskar Rojek <s25723@pjwstk.edu.pl>
2023-11-25 20:25:32 +0100

    Added Fuel Bar

--- Assets/Scripts/Player/PlayerEvents.cs
+++ Assets/Scripts/Player/PlayerEvents.cs
@@ -2,8 +2,13 @@ using System;
 
 public static class PlayerEvents
 {
+    public static event Action<float> OnFuelUpdate;
     public static event Action OnFuelRefill;
 
+    public static void UpdateFuel(float currentFuelAmount)
+    {
+        OnFuelUpdate?.Invoke(currentFuelAmount);
+    }
     public static void RefillFuel()
     {
         OnFuelRefill?.Invoke();
