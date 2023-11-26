601c67635ff1aa335e3400a774bef678c4ae6d4a
Juliusz <s23751@pjwstk.edu.pl>
2023-11-26 01:34:17 +0100

    Changes to some files

--- Assets/ScriptsFromIngredientMerge/PlayerMovementOld.cs
+++ Assets/ScriptsFromIngredientMerge/PlayerMovementOld.cs
@@ -1,52 +1,50 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class PlayerMovement : MonoBehaviour
+namespace ScriptsFromIngredientMerge
 {
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerMovementOld : MonoBehaviour
+    {
 
-    private Rigidbody2D _rigidbody2D;
-    private Hand _hand;
-    [SerializeField] private float _playerMovementSpeed;
+        private Rigidbody2D _rigidbody2D;
+        private Hand _hand;
+        [SerializeField] private float _playerMovementSpeed;
     
-    private void Start()
-    {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-        _hand = GetComponentInChildren<Hand>();
-    }
-    
-    private void Update()
-    {
-        //player movement left/right
-        var horizontalInput = Input.GetAxis("Horizontal");
-        _rigidbody2D.velocity = new Vector2(horizontalInput * _playerMovementSpeed, _rigidbody2D.velocity.y);
-        CatchFood();
-    }
-
-    public void CatchFood()
-    {
-        Collider2D[] foodInRange = Physics2D.OverlapCircleAll(
-            _hand.CatchZone.position,
-            _hand.CatchZoneRange,
-            _hand.FoodLayerMask
-        );
-        
-        if (foodInRange.Length > 0)
+        private void Start()
         {
-            foreach (Collider2D foodCollider in foodInRange)
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+            _hand = GetComponentInChildren<Hand>();
+        }
+    
+        private void Update()
+        {
+            //player movement left/right
+            var horizontalInput = Input.GetAxis("Horizontal");
+            _rigidbody2D.velocity = new Vector2(horizontalInput * _playerMovementSpeed, _rigidbody2D.velocity.y);
+            CatchFood();
+        }
+
+        public void CatchFood()
+        {
+            Collider2D[] foodInRange = Physics2D.OverlapCircleAll(
+                _hand.CatchZone.position,
+                _hand.CatchZoneRange,
+                _hand.FoodLayerMask
+            );
+        
+            if (foodInRange.Length > 0)
             {
-                if (Input.GetMouseButtonDown(0))
+                foreach (Collider2D foodCollider in foodInRange)
                 {
-                    _hand.PickUpFood(foodCollider.gameObject);
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        _hand.PickUpFood(foodCollider.gameObject);
+                    }
                 }
             }
         }
-    }
     
-    /*private void OnTriggerEnter2D(Collider2D other)
+        /*private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Food"))
         {
@@ -59,4 +57,5 @@ public class PlayerMovement : MonoBehaviour
     }
     */
     
+    }
 }
